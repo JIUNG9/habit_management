06d6e68369c39b6e8ba4ab0a724ed48a1db00d49
Boheon <86608286+Boheon@users.noreply.github.com>
2022-12-02 14:57:29 +0900

    aws 데이터베이스 추가 서브모듈 bootjar 메인파일 생성제거 aws beanstalk 포트번호 5000으로 변경

--- src/main/java/com/module/UserManagementApplication.java
+++ src/main/java/com/module/UserManagementApplication.java
@@ -1,10 +1,12 @@
 package com.module;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Slf4j
 @EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan(basePackages = "com.module.*")
--- src/main/java/com/module/service/impl/HabitServiceImpl.java
+++ src/main/java/com/module/service/impl/HabitServiceImpl.java
@@ -16,12 +16,11 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
-import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @EnableAsync
@@ -30,6 +29,7 @@ public class HabitServiceImpl implements HabitService {
     private HabitRepository habitRepository;
     private HabitCategoryRepository habitCategoryRepository;
     private UserRepository userRepository;
+
 
     public HabitServiceImpl(HabitRepository habitRepository, HabitCategoryRepository habitCategoryRepository, UserRepository userRepository){
         this.habitRepository = habitRepository;
@@ -146,7 +146,7 @@ public class HabitServiceImpl implements HabitService {
                 .categoryName(habit.getHabitCategory().getHabitCategoryName())
                 .build();
 
-        return myPeriodResponse;
+        return myPeriodResponse;//초단위로 보내는거임
     }
 
     @Override
@@ -224,54 +224,47 @@ public class HabitServiceImpl implements HabitService {
         habitRepository.delete(habit);
     }
 
-    public List<Long> getTotalPeriodCount(List<Long> totalPeriods){
+    public List<Long> getTotalPeriodCount(List<Long> totalPeriods){//totalPeriod 초단위임
         long count1 = 0; long count2 = 0; long count3 = 0; long count4 = 0;
         long count5 = 0; long count6 = 0; long count7 = 0; long count8 = 0;
         long count9 = 0; long count10 = 0; long count11= 0; long count12 = 0;
-        long count13 = 0;
-
-
-        List<Long> dividedBy2629700 = totalPeriods.stream().map(t->t/2629700).collect(Collectors.toList());
-
-        for(long totalPeriod : dividedBy2629700){
+        List<Long> totalPeriodsMonth = totalPeriods.stream().map(i -> TimeUnit.SECONDS.toDays(i)/30).collect(Collectors.toList());
+        for(long totalPeriod : totalPeriods){
             if(totalPeriod>=0 && totalPeriod < 1){
-               count1 = count1++;
+                count1 += 1;
             }
             else if(totalPeriod>=1 && totalPeriod < 2){
-                count2 = count2++;
+                count2 += 1;
             }
             else if(totalPeriod>=2 && totalPeriod < 3){
-                count3 = count3++;
+                count3 += 1;
             }
             else if(totalPeriod>=3 && totalPeriod < 4){
-                count4 = count4++;
+                count4 += 1;
             }
             else if(totalPeriod>=4 && totalPeriod < 5){
-                count5 = count5++;
+                count5 += 1;
             }
             else if(totalPeriod>=5 && totalPeriod < 6){
-                count6 = count6++;
+                count6 += 1;
             }
             else if(totalPeriod>=6 && totalPeriod < 7){
-                count7 = count7++;
+                count7 += 1;
             }
             else if(totalPeriod>=7 && totalPeriod < 8){
-                count8 = count8++;
+                count8 += 1;
             }
             else if(totalPeriod>=8 && totalPeriod < 9){
-               count9 = count9++;
+                count9 += 1;
             }
             else if(totalPeriod>=9 && totalPeriod < 10){
-                count10 = count10++;
+                count10 += 1;
             }
             else if(totalPeriod>=10 && totalPeriod < 11){
-                count11 = count11++;
+                count11 += 1;
             }
             else if(totalPeriod>=11 && totalPeriod < 12){
-                count12 +=1;
-            }
-            else if (totalPeriod >= 13) {
-                count13 +=1;
+                count12 += 1;
             }
         }
 
@@ -279,58 +272,58 @@ public class HabitServiceImpl implements HabitService {
                                                     count5, count6, count7, count8,
                                                     count9, count10, count11, count12);
 
+        System.out.println(totalPeriodCount.toString());
         return totalPeriodCount;
     }
 
     public List<Long> getTotalAmountCount(List<Long> totalAmounts){
         long count1 = 0; long count2 = 0; long count3 = 0; long count4 = 0;
         long count5 = 0; long count6 = 0; long count7 = 0; long count8 = 0;
-        long count9 = 0; long count10 = 0; long count11= 0; long count12 = 0;
+        long count9 = 0; long count10 = 0; long count11= 0;
         long max = Collections.max(totalAmounts);
         double gap = max/10;
 
-        for(long totalPeriod : totalAmounts){
-            if(totalPeriod>=0 && totalPeriod < gap){
-                count1 = count1++;
+        for(long totalAmount : totalAmounts){
+            if(totalAmount>=0 && totalAmount< gap){
+                count1 += 1;
             }
-            else if(totalPeriod>=gap && totalPeriod < 2*gap){
-                count2 =count2++;
+            else if(totalAmount>=gap && totalAmount < 2*gap){
+                count2 += 1;
             }
-            else if(totalPeriod>=2*gap && totalPeriod < 3*gap){
-                count3 = count3++;
+            else if(totalAmount>=2*gap && totalAmount < 3*gap){
+                count3 += 1;
             }
-            else if(totalPeriod>=3*gap && totalPeriod < 4*gap){
-                count4 = count4++;
+            else if(totalAmount>=3*gap && totalAmount < 4*gap){
+                count4 += 1;
             }
-            else if(totalPeriod>=4*gap && totalPeriod < 5*gap){
-                count5 = count5++;
+            else if(totalAmount>=4*gap && totalAmount < 5*gap){
+                count5 += 1;
             }
-            else if(totalPeriod>=5*gap && totalPeriod < 6*gap){
-                count6 = count6++;
+            else if(totalAmount>=5*gap && totalAmount< 6*gap){
+                count6 += 1;
             }
-            else if(totalPeriod>=6*gap && totalPeriod < 7*gap){
-               count7 = count7++;
+            else if(totalAmount>=6*gap && totalAmount < 7*gap){
+                count7 += 1;
             }
-            else if(totalPeriod>=7*gap && totalPeriod < 8*gap){
-                count8 =count8++;
+            else if(totalAmount>=7*gap && totalAmount < 8*gap){
+                count8 += 1;
             }
-            else if(totalPeriod>=8*gap && totalPeriod < 9*gap){
-                count9 = count9++;
+            else if(totalAmount>=8*gap && totalAmount < 9*gap){
+                count9 += 1;
             }
-            else if(totalPeriod>=9*gap && totalPeriod < 10*gap){
-               count10 += 1;
-
-            } else if (totalPeriod >= 10 * gap && totalPeriod < 11 * gap) {
-                count11 +=1;
+            else if(totalAmount>=9*gap && totalAmount < 10*gap){
+                count10 += 1;
             }
-
-
+            else if(totalAmount>=10*gap && totalAmount < 11*gap) {
+                count11 += 1;
+            }
         }
 
         List<Long> totalAmountCount = Arrays.asList(count1, count2, count3, count4,
                 count5, count6, count7, count8,
-                count9, count10, count11, count12);
+                count9, count10, count11);
 
+        System.out.println(totalAmountCount.toString());
         return totalAmountCount;
     }
 
