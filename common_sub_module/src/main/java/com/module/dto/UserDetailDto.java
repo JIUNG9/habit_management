ede94bfa4e0ed246f11c6e64819908a5dd4ec124
UNG <rnwldnd7248@gmail.com>
2022-12-03 16:23:04 +0900

    Feat: Add the role when getting user info

--- common_sub_module/src/main/java/com/module/dto/UserDetailDto.java
+++ common_sub_module/src/main/java/com/module/dto/UserDetailDto.java
@@ -1,5 +1,6 @@
 package com.module.dto;
 
+import com.module.type.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,5 @@ public class UserDetailDto {
 
     String email;
     String name;
+    Role role;
 }
