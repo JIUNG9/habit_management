7ede2241b8c4d5293f5dfa8a0a56452aa5b418e0
Boheon <86608286+Boheon@users.noreply.github.com>
2022-12-04 14:48:10 +0900

    lombok.config 추가 @Data와 @Builder같이 있을때 private 기본 생성자 생성안해주는거 해결

--- common_sub_module/src/main/java/com/module/dto/HabitDTO.java
+++ common_sub_module/src/main/java/com/module/dto/HabitDTO.java
@@ -1,10 +1,7 @@
 package com.module.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
@@ -12,8 +9,6 @@ import java.sql.Timestamp;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class HabitDTO {
     private Long id;
 
